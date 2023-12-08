108d787502b8eb17b06953904b537eaaeda9f78c
yycorcino <corcinosebastian@outlook.com>
2023-12-07 15:17:50 -0900

    build template for a* algorithm

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def shuffleGrid(grid_data):
 def validateGrid(grid_data):
     """
     verifies that a shuffled grid is solvable by transforming
-    a 1D array and contains an even number of inversions.
+    a 1D array and contains an even number of inversions
     """
    
     # inversions consists of a current position and every position after it
@@ -23,25 +23,38 @@ def validateGrid(grid_data):
     array = np.array(grid_data).flatten()
     
     count = 0
-    for i in range(len(array)):
-        for j in range(len(array)):
+    for i in range(len(array) - 1):
+        for j in range(i + 1, len(array)):
             if (array[i] > array[j]):
                 count += 1
     
-    return True
+    return True if count % 2 == 0 else False
 
-def solve(grid_data):
+def hScore(shuffle_grid, goal_grid):
+    """
+    heuristic function that compares the current state and the goal state
+    h-score will be determine by the number of misplaced tiles 
+    """
+
+    print("heuristic value")
+
+     
+def solve(shuffle_grid, goal_grid):
+    g_score = 0 # number of moves
+    f_score = 0 # g_score + h_score
+
     print("solving...")
-    return grid_data
+    return shuffle_grid
 
 if __name__ == '__main__':
-    grid_data = [
+    goal_grid = [
             ["1", "2", "3"],
             ["4", "5", "6"],
-            ["7", "8", "0"]
+            ["7", "8", "0"],
         ]
     
-    shuffle_grid = shuffleGrid(grid_data)
+    shuffle_grid = shuffleGrid(goal_grid)
+    solve(shuffle_grid, goal_grid)
     
     
 
