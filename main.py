7c7bc0203691f32a88521fcf4437354f332a014b
yycorcino <corcinosebastian@outlook.com>
2023-12-10 03:05:37 -0900

    solves simple scrambles

--- main.py
+++ main.py
@@ -1,115 +1,165 @@
-import copy
+from copy import deepcopy
 import random
-def shuffleGrid(grid_data, n):
-    """
-   transform 2D array into 1D array, then shuffles and returns
-   shuffle 2D array
-    """
-    for i in range(n - 1):
-        random.shuffle(grid_data[i])
+import numpy as np
 
-    random.shuffle(grid_data)
+class GridNode:
+    def __init__(self, grid, g_score = 0, f_score = 0):
+        """
+        graph instance contain the grid, g_score which is the amount of moves, 
+        and f_score which is g_score + h_score
+        """
+        self.grid = grid
+        self.g_score = g_score
+        self. f_score = f_score
+
+    def nextMoves(self):
+        """
+        create children to be able to choose the best child 
+        """
+        # possible moves
+        x, y = self.findCoord(0)
+        move_up = [x, y - 1]
+        move_down = [x, y + 1]
+        move_left = [x - 1, y]
+        move_right = [x + 1, y]
+
+        children = []
+        for move in [move_up, move_down, move_left, move_right]:
+            child = self.verifyMove(x, y, move[0], move[1])
+
+            if child is None:
+                continue
+            
+            new_GridNode = GridNode(child, self.g_score + 1, 0)
+            children.append(new_GridNode)
+
+            # print(f"move: {move}")
+            # print(new_GridNode.grid)
+
+        return children
     
-    return grid_data
+    def verifyMove(self, curr_coord_x, curr_coord_y, future_coord_x, future_coord_y):
+        """
+        test the blank tile in new directional tile and verify
+        within bounds of the grid
+        """
+        if (0 <= future_coord_x < len(self.grid) and
+            0 <= future_coord_y < len(self.grid)):
 
+            # switch blank tile with tile in its new position
+            temp_grid = deepcopy(self.grid)
+            temp_val = temp_grid[future_coord_x][future_coord_y]
+            temp_grid[future_coord_x][future_coord_y] = temp_grid[curr_coord_x][curr_coord_y]
+            temp_grid[curr_coord_x][curr_coord_y] = temp_val
+
+            return temp_grid
+        else:
+            return None
      
-def solve(shuffle_grid, goal_grid):
-    print("solving...")
-    return a_star(shuffle_grid, goal_grid)
 
-class Node:
-    def __init__(self,data,level,f_score):
-        """ Initialize the node with the data, level of the node and the calculated fvalue """
-        self.data = data
-        self.level = level
-        self.f_score = f_score
+    def findCoord(self, target_val):
+        """
+        determine coordinate points of provided value
+        """
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid)):
+                if self.grid[i][j] == target_val:
+                    return [i, j]
 
-def get_f_score(start, goal):
-    score = 0
-    n = len(start.data)
+class SlidingPuzzle:
+    def __init__(self, n):
+        """
+        sliding puzzle instance which track the tree that being created
+        from the available moves and choice of best move
+        """
+        self.n = n
+        self.open_list = []
+        self.close_list = []
 
-    for i in range(0, n):
-        for j in range(0, n):
-            if start.data[i][j] != goal[i][j] and start [i][j] != 0:
-                score += 1
+    def generateStartGrid(self):
+        """
+       create a list of random value from 0 - n then reshape 
+       into a n x n grid
+        """
+        values = np.arange(self.n**2)
+        np.random.shuffle(values)
+       
+        return values.reshape((self.n, self.n))
+    
+    def generateGoalGrid(self):
+        """
+        create a list of values from 1 - n and 0 then reshape
+        into a n x n grid
+        """
+        values = np.arange(1, self.n**2)
+        values = np.append(values, 0)
 
-    return score
+        return values.reshape((self.n, self.n))
 
-def get_children(current_node):
-    x, y = current_node.find(current_node.data, '0')
+    def hScore(self, curr_grid, goal_grid):
+        """
+        h-score will be determine by the number of misplaced tiles 
+        """  
+        if np.array_equal(curr_grid, goal_grid):
+            return 0
 
-    children = []
-    moves = [[x,y-1],[x,y+1],[x-1,y],[x+1,y]]
+        misplaced_coord= np.where(curr_grid != goal_grid)
+        misplaced = len(misplaced_coord[0])
+        
+        return misplaced
+    
+    def fScore(self, curr_GridNode, goal_grid):
+        g_score = curr_GridNode.g_score
+        h_score = self.hScore(curr_GridNode.grid, goal_grid)
 
-    for move in moves:
-        child = move(current_node.data, x, y, move[0], move[1])
-        if child is not None:
-                child_node = Node(child, current_node.level + 1, 0)
-                children.append(child_node)
-    return children
+        return g_score + h_score
 
-def move(node_data, x1, y1, x2, y2):
-    if x2 >= 0 and x2 < len(node_data) and y2 >= 0 and y2 < len(node_data):
-        temp_data = []
-        temp_data = copy(node_data)
-        temp = temp_data[x2][y2]
-        temp_data[x2][y2] = temp_data[x1][y1]
-        temp_data[x1][y1] = temp
-        return temp_data
-    return None
+    def solve(self):
+        start_grid = self.generateStartGrid() 
+        goal_grid = self.generateGoalGrid()
+        # start_grid = np.array([
+        #         [1, 2, 3],
+        #         [0, 4, 6],
+        #         [7, 5, 8]
+        #     ])
+        # goal_grid = np.array( [
+        #         [1, 2, 3],
+        #         [4, 5, 6],
+        #         [7, 8, 0]
+        #     ])
 
+        start_GridNode = GridNode(start_grid)
+        start_GridNode.f_score = self.fScore(start_GridNode, goal_grid)
+        self.open_list.append(start_GridNode)
 
-def a_star(init_state, goal_state):
-    n_size = len(init_state)
-    open_list = []
-    closed_list = []
+        #
+        while True:
+            current_GridNode = self.open_list[0]
+            print("Current Grid:")
+            print(current_GridNode.grid)
+            print("X-----X-----X")
 
-    start = Node(init_state,0,0)
-    start.f_score = get_f_score(start, goal_state)
-    """ Put the start node in the open list"""
-    open_list.append(start)
-    print("\n\n")
-    while True:
-        cur = open_list[0]
-        print("")
-        print("  | ")
-        print("  | ")
-        print(" \\\'/ \n")
-        for i in cur.data:
-            for j in i:
-                print(j,end=" ")
-            print("")
-        """ If the difference between current and goal node is 0 we have reached the goal node"""
-        if(get_f_score(cur.data, goal_state) == 0):
-            break
-        for i in cur.get_children():
-            i.f_score = get_f_score(i, goal_state)
-            open_list.append(i)
-        closed_list.append(cur)
-        open_list.pop(0)
+            # h_score is 0 because no misplaced tiles exist 
+            if self.hScore(current_GridNode.grid, goal_grid) == 0:
+                break
 
-        """ sort the opne list based on f value """
-        open_list.sort(key = lambda x:x.f_score, reverse=False)
+            # create children
+            for child_GridNode in current_GridNode.nextMoves():
+                # print("printing childGridNode")
+                # print(child_GridNode.grid)
+                child_GridNode.f_score = self.fScore(child_GridNode, goal_grid)
+                self.open_list.append(child_GridNode)
+
+            # print("done printing childGridNode")
+            #
+            self.close_list.append(current_GridNode)
+            del self.open_list[0]
+          
+            #self.open_list.sort(key = lambda x:x.f_score, reverse=False)
+        
 
 if __name__ == '__main__':
-    max_iter = 5000
-    heuristic = "linear_conflict"
-    algorithm = "a_star"
-    n = 3
-    
-    goal_state = [
-            [1, 2, 3],
-            [4, 5, 6,],
-            [7, 8, 0],
-        ]
-    
-    #goal_state = [
-    #        [1, 2, 3, 4],
-    #        [5, 6, 7, 8],
-    #        [9, 10, 11, 12],
-    #        [13, 14, 15, 0],
-    #    ]
-    
-    initial_state = shuffleGrid(goal_state, n)
-    
-    solve(initial_state, goal_state)
+    game = SlidingPuzzle(3)
+    game.solve()
+
+  
